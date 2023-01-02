568d69566bb7a77a703c1af7b570c2cdc02f0a21
simimi129 <simimi129@gmail.com>
2023-01-02 12:24:06 +0100

    adding cart context+functionality

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
-function MealItem({ name, description, price }) {
+function MealItem({ id, name, description, price }) {
+  const cartCtx = useContext(CartContext);
+
   const formattedPrice = `$${price.toFixed(2)}`;
+
+  function addToCartHandler(amount) {
+    cartCtx.addItem({
+      id: id,
+      name: name,
+      amount: amount,
+      price: price,
+    });
+  }
 
   return (
     <li className={classes.meal}>
@@ -13,7 +25,7 @@ function MealItem({ name, description, price }) {
         <div className={classes.price}>{formattedPrice}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
