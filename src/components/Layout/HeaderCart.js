75887c42989a8d979029921056676087e9e9347e
simimi129 <simimi129@gmail.com>
2023-01-02 13:09:48 +0100

    adding cart context+functionality

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,17 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCart.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 function HeaderCart({ onClick }) {
+  const [btnIsBump, setBtnIsBump] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((current, item) => {
+  const numberOfCartItems = items.reduce((current, item) => {
     return current + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsBump(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
