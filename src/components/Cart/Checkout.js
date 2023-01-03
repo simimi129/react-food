ceb762fd0977f956bd6b3bd3dc27a48272854d31
simimi129 <simimi129@gmail.com>
2023-01-03 16:29:50 +0100

    tweaking checkout

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classes from "./Checkout.module.css";
 
-function Checkout({ onCancel }) {
-  function confirmHandler(event) {
+const Checkout = (props) => {
+  const confirmHandler = (event) => {
     event.preventDefault();
-  }
+  };
 
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" />
@@ -24,12 +24,14 @@ function Checkout({ onCancel }) {
         <label htmlFor="city">City</label>
         <input type="text" id="city" />
       </div>
-      <button type="button" onClick={onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
-}
+};
 
 export default Checkout;
