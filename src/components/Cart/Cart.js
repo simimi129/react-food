292c524436d63f80d49edfd45861d888fa369ce0
simimi129 <simimi129@gmail.com>
2023-01-03 15:24:40 +0100

    adding checkout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 function Cart({ onClose }) {
+  const [isCheckout, setIsCheckout] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -16,6 +18,10 @@ function Cart({ onClose }) {
 
   function cartItemRemoveHandler(id) {
     cartCtx.removeItem(id);
+  }
+
+  function orderHandler() {
+    setIsCheckout(true);
   }
 
   return (
@@ -36,12 +42,19 @@ function Cart({ onClose }) {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onCancel={onClose} />}
+      {!isCheckout && (
+        <div className={classes.actions}>
+          <button className={classes["button--alt"]} onClick={onClose}>
+            Close
+          </button>
+          {hasItems && (
+            <button className={classes.button} onClick={orderHandler}>
+              Order
+            </button>
+          )}
+        </div>
+      )}
     </Modal>
   );
 }
